90cdc70c6fb1980f8471bf70788c8bf0833485c4
iGaryBot <49323718+iGaryBot@users.noreply.github.com>
2019-04-08 15:32:13 -0500

    initial commit

---
+++ TIMEStiCkiNg.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IHATEsKyeMains;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Robotics
+ */
+public class TIMEStiCkiNg {
+    public static void main (String[] args){
+System.out.println("Hello World!!\n");
+   Scanner scanner = new Scanner(System.in);
+   System.out.println("What's your name?");
+  String name = scanner.next();
+        System.out.println("Hello "+ name
+                +"I HATE YOUR GUTS AND WILL KILL YOU");
+   }}
